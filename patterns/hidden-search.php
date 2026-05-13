d797db4c9278381cd2c067fbe471a10339f8037f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-13 18:33:52 -0300

    feat: first commit
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ patterns/hidden-search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: libresign/hidden-search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'libresign' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'libresign' ); ?>","fontSize":"medium"} /-->
